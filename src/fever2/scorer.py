dff9cb62aff9800132b7cc947c713504c2295f50
James Thorne <james@jthorne.co.uk>
2021-08-16 11:30:09 +0100

    Update scorer.py

--- src/fever2/scorer.py
+++ src/fever2/scorer.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from fever.scorer import fever_score
 
 
@@ -8,26 +9,74 @@ def potency(actual, *all_systems_predictions):
     return sum(system_scores)/len(all_systems_predictions)
 
 
+def resilience(actual, predictions):
+    return fever_score(predictions, actual)[0]
+
+
+
 if __name__ == "__main__":
     import argparse, json, os
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--actual-file")
-    parser.add_argument("--submission-dir")
+    parser.add_argument("--actual-files")
+    parser.add_argument("--breaker-dirs")
+    parser.add_argument("--correct-scores")
     args = parser.parse_args()
 
-    all_submissions = []
-    actual = []
+    actual_part = []
+    all_breakers_actual = []
+    all_systems = defaultdict(list)
 
-    for filename in os.listdir(args.submission_dir):
-        with open(args.submission_dir+"/"+filename,"r") as file:
-            file_lines = []
-            for line in file:
-                file_lines.append(json.loads(line))
-            all_submissions.append(file_lines)
 
-    with open(args.actual_file, "r") as file:
-        for line in file:
-            actual.append(json.loads(line))
+    final_scores = [float(score) for score in args.correct_scores.split(",")]
+    system_potencies = {}
 
-    print(potency(actual,all_submissions))
+    for corr_score, actual_file, breaker in zip(final_scores, args.actual_files.split(","), args.breaker_dirs.split(",")):
+        all_submissions = []
+        actual = []
+
+        for filename in os.listdir(breaker):
+            with open(breaker+"/"+filename,"r") as file:
+                file_lines = []
+                for line in file:
+                    line_text = json.loads(line)
+                    if not "predicted_evidence" in line_text and "predicted_sentences" in line_text:
+                       line_text["predicted_evidence"] = line_text["predicted_sentences"]
+                    file_lines.append(line_text)
+                all_submissions.append(file_lines)
+                all_systems[filename].append(file_lines)
+
+        with open(actual_file, "r") as file:
+            for idx,line in enumerate(file):
+                try:
+                    actual.append(json.loads(line))
+                except Exception as e:
+                    print(idx,line)
+                    raise e
+            all_breakers_actual.extend(actual)
+            actual_part.append(actual)
+
+        p_score = potency(actual,*all_submissions)
+        print("Breaker {0}. Potency: {1}. Correct Rate: {2}. Adjusted Potency: {3}".format(breaker,p_score, corr_score, p_score*corr_score))
+
+
+    for system_name in all_systems.keys():
+        system_predictions = all_systems[system_name]
+        scores = []
+        for breaker,breaker_correctness, breaker_actual, breaker_predictions in zip(args.breaker_dirs.split(","),final_scores,actual_part, system_predictions):
+            print("Breaker {} - System {}".format(breaker,system_name))
+            print(list(zip(["fever","acc","p","r","f1"],fever_score(breaker_predictions, breaker_actual))))
+            scores.append(fever_score(breaker_predictions, breaker_actual)[0]*breaker_correctness)
+
+        print("System {0}. Resilience {1}".format(system_name, sum(scores)/sum(final_scores)))
+        print(list(zip(args.actual_files.split(","),scores)))
+        print()
+    print(" & ".join(args.breaker_dirs.split(",")))
+    for system_name in all_systems.keys():
+        system_predictions = all_systems[system_name]
+        scores = []
+        for breaker_correctness, breaker_actual, breaker_predictions in zip(final_scores,actual_part, system_predictions):
+            scores.append(fever_score(breaker_predictions, breaker_actual)[0])
+
+
+        print(" & ".join([system_name]+[str(round(a*100,2)) for a in scores])+ " \\\\")
